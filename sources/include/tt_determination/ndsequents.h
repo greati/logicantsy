f0b3692ccec60b1926a8cee7756eb537bd401dcc
Vitor Greati <greati@ufrn.edu.br>
2020-07-01 00:50:17 -0300

    Fix case where connective does not appear

--- sources/include/tt_determination/ndsequents.h
+++ sources/include/tt_determination/ndsequents.h
@@ -43,13 +43,15 @@ namespace ltsy {
                 } 
             }
 
-            void _determine_by_dimension(int dimension, const NdSequent<std::set>& sequent, 
+            bool _determine_by_dimension(int dimension, const NdSequent<std::set>& sequent, 
                     std::set<Determinant<std::unordered_set<int>>>& determinants) {
                 for (int i = 0; i < _props.size(); ++i) {
                     Formula& p = _props[i];
                     if (sequent.is_in(dimension, p))
                         _remove_determinants_by_pos_attitude(_attitudes[dimension], i, determinants);
                 }
+                std::cout << "After propositional" << std::endl;
+                std::cout << determinants << std::endl;
                 if (sequent.is_in(dimension, *_compound)) {
                     for (auto it = determinants.begin(); it != determinants.end();) {
                         auto det = (*it);
@@ -57,15 +59,29 @@ namespace ltsy {
                         it = determinants.erase(it);
                         determinants.insert(it, det);
                     }
+                    return true;
                 }
+                return false;
             }
 
             void _determine_by_sequent(const NdSequent<std::set>& sequent) {
                 auto dimension = sequent.dimension();     
                 auto counter_model_determinants = generate_fully_partial_table(_nvalues, _connective.arity()).get_determinants();
+                bool compound_has_appeared = false;
                 // produce counter-model determinants
                 for (auto i = 0; i < dimension; ++i)
-                    _determine_by_dimension(i, sequent, counter_model_determinants); 
+                    compound_has_appeared |= _determine_by_dimension(i, sequent, counter_model_determinants); 
+                // if the compound has not appeared, set the counter-model determinants to be fully ND
+                if (not compound_has_appeared)
+                    for (auto it = counter_model_determinants.begin(); it != counter_model_determinants.end();) {
+                        auto det = (*it);
+                        std::unordered_set<int> s;
+                        for (int i = 0; i < _nvalues; ++i)
+                            s.insert(i);
+                        det.set_last(s);
+                        it = counter_model_determinants.erase(it);
+                        counter_model_determinants.insert(it, det);
+                    }
                 // update table based on determination
                 _table.update(counter_model_determinants, 
                         [](std::unordered_set<int> current, std::unordered_set<int> counter_models) -> std::unordered_set<int> {
--- sources/tests/truth_table_determination.cpp
+++ sources/tests/truth_table_determination.cpp
@@ -125,4 +125,28 @@ namespace {
         std::cout << table << std::endl;
     }
 
+    TEST(Determinants, TopRemoval) {
+        auto p = std::make_shared<ltsy::Prop>("p");
+        auto q = std::make_shared<ltsy::Prop>("q");
+        auto conn = std::make_shared<ltsy::Connective>("&", 2);
+        auto p_conn_q = std::make_shared<ltsy::Compound>(conn, std::vector<std::shared_ptr<ltsy::Formula>>{p, q});
+
+        ltsy::NdSequent<std::set> seq1 ({{},{},{p},{p}});
+        ltsy::NdSequent<std::set> seq2 ({{},{},{q},{q}});
+        std::vector<ltsy::NdSequent<std::set>> sequents = {seq1, seq2};
+
+        ltsy::CognitiveAttitude a {"A", {2, 3}};
+        ltsy::CognitiveAttitude na {"NA", {0, 1}};
+        ltsy::CognitiveAttitude nr {"NR", {1, 3}};
+        ltsy::CognitiveAttitude r {"R", {0, 2}};
+
+        ltsy::NdSequentTruthTableDeterminizer determinizer {4, {*p, *q}, *conn, {nr, na, r, a}};
+
+        determinizer.determine(sequents);
+
+        auto table = determinizer.table();
+
+        std::cout << table << std::endl;
+    }
+
 };
